6c8c33b718a8ffbde3894ff39a68ff47fd7db2e0
ssebasarias <guerreroarias20@gmail.com>
2025-12-19 11:41:30 -0500

    actualizacion en archvo de estructura BD

--- docs/DB_UPDATE_V7_HIERARCHY.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE products
-ADD COLUMN IF NOT EXISTS taxonomy_concept VARCHAR(255),
-ADD COLUMN IF NOT EXISTS taxonomy_industry VARCHAR(255),
-ADD COLUMN IF NOT EXISTS taxonomy_level VARCHAR(50);
--- concept: "Silla Gamer", industry: "Muebles", level: "CONCEPT"
-
-CREATE INDEX IF NOT EXISTS idx_products_taxonomy_concept ON products(taxonomy_concept);
